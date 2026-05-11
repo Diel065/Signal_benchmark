38a31fa2dc26c0706e14d7237bdcf585528d909e
Daniel L <daniel.labuda@study.hs-duesseldorf.de>
2026-05-11 22:51:50 +0200

    Delete src directory

--- src/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("Paste into terminals to run:");
-    println!("  cargo run --bin key_repository");
-    println!("  cargo run --bin client -- --name alice --key-repository-url http://127.0.0.1:3000");
-}
